0105dd95386144981ccebef730c2a927250e64b9
Gonza <gnzx89@gmail.com>
2024-07-31 15:40:13 -0300

    ..

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState } from 'react';
+import { useState , useEffect } from 'react';
 import { motion } from 'framer-motion';
 import Candidato from './Componentes/candidato';
 import candidatos from "./candidatos.json";
@@ -9,7 +9,6 @@ function App() {
   const [totalVotos , setTotalVotos] = useState(0);
   const [votosMilei , setVotosMilei] = useState(0);
   const [votosMassa , setVotosMassa] = useState(0);
-  const [porcentaje , setPorcentaje] = useState(0);
 
   const recibirVotos = (votoRecibido) => {setTotalVotos(totalVotos + 1)}
 
@@ -22,24 +21,23 @@ function App() {
   return (
     <div className="App">
       <h1 className='titulo-app'>Resultados electorales</h1>
-      <h2>Lista de candidatos</h2>
-      <div className='bloque-resultados'>
+      <div className= {votosMassa > votosMilei ? 'bloque-resultados ascenso' : 'bloque-resultados'}>
         {candidatos.map(candidato => (
           <Candidato 
           nombre={candidato.nombre} 
           imagen={candidato.imagen}
+          id = {candidato.nombre}
           enviarVotos = {recibirVotos}
           totalVotos = {totalVotos}
           enviarId = {recibirId}/>
         ))}
       </div>
-          
           <span className='contador-caja'>Total de votantes : <motion.div className='contador-contenedor'
           key={totalVotos}
           initial={{ opacity: 0, y: 50 , scale : 0}}
           animate={{ opacity: 1, y: 0 , scale : 1 }}
           exit={{ opacity: 0, y: -50 }}
-          transition={{ duration: .25 }}>{totalVotos}</motion.div> {totalVotos > 1 || totalVotos == 0  ? 'Electores' : 'Elector'}</span>
+          transition={{ duration: .25 }}>{totalVotos}</motion.div> {totalVotos > 1 || totalVotos === 0  ? 'Electores' : 'Elector'}</span>
     </div>
   );
 }
--- src/Componentes/candidato.jsx
+++ src/Componentes/candidato.jsx
@@ -1,7 +1,7 @@
-import { React, useState, useEffect } from "react";
+import { React, useState} from "react";
 import { motion } from 'framer-motion';
 import "../Estilos/candidato.css";
-import { Transition } from "react-transition-group";
+
 
 const Candidato = (props) => {
 
@@ -9,7 +9,7 @@ const Candidato = (props) => {
   const [votosMilei , setVotosMilei] = useState(0);
   const [votosMassa , setVotosMassa] = useState(0);
 
-  const [animacionActiva, setAnimacionActiva] = useState(false);
+
 
   const validarId = () => {
     props.nombre === 'javier milei' ? 
@@ -54,14 +54,20 @@ const Candidato = (props) => {
     0 : definirPorcentaje(props.nombre)
 
   return (
-    <div className="contenedor-candidato">
+    <div className='contenedor-candidato'>
+      <div className="nombre-votos-contenedor">
       <h2 className="nombre-candidato">{props.nombre}</h2>
+      <h3 className="votosporpartido">
+        {props.nombre === 'javier milei' ? 
+        votosMilei : votosMassa} 
+        {(votosMilei || votosMassa) > 1 || votosMilei || votosMassa === 0 ? 'votos' : 'voto'}</h3>
+      </div>
       <div className="contenedor-partido">
         <button className="contenedor-imagen" onClick={sumarYEnviar}>
           <img className="imagen-partido " src={props.imagen} alt="" />
         </button>
         <div className="contenedor-barraporcentual">
-            <h3>{porcentaje === undefined ? 'vacio' : porcentaje.toFixed(2)}%</h3>
+            <h3>{porcentaje.toFixed(2)}%</h3>
           <motion.div     
             className={
               props.nombre === "javier milei"
@@ -77,7 +83,6 @@ const Candidato = (props) => {
         
         
       </div>
-      <h3 className="votosporpartido">Votos por <span>{props.nombre}</span>: {props.nombre === 'javier milei' ? votosMilei : votosMassa}</h3>
     </div>
   );
 };
