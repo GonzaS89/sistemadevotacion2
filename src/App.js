75db6384b663eff29b6ad64d69de340d06334594
GonzaS89 <gnzx89@gmail.com>
2024-08-02 02:01:20 -0300

    resolucion celu lista

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-
+import { useState } from 'react';
 import Pantallavotacion from './Componentes/Pantallavotacion';
 
 
 function App() {
 
+  
   return (
     <div className="App">
        <h1 className='titulo-app'>Resultados electorales</h1>
--- src/Componentes/Pantallavotacion.jsx
+++ src/Componentes/Pantallavotacion.jsx
@@ -6,24 +6,40 @@ import Totaldevotantes from './Totaldevototantes';
 import Votos from './Votos';
 import '../Estilos/pantallavotacion.css'
 
-const Pantallavotacion = () => {
+const Pantallavotacion = (props) => {
 
     
     const [totalVotos , setTotalVotos] = useState(0);
     const [votosMilei , setVotosMilei] = useState(0);
     const [votosMassa , setVotosMassa] = useState(0);
+    const [condicionVotosTotales, setCondicionVotosTotales] = useState(false);
+
     const [id , setID] = useState('');
 
     useEffect(() => {
       setTotalVotos(votosMilei + votosMassa)
-    },[votosMilei , votosMassa])
+    },[votosMilei , votosMassa]);
+
+    useEffect(() => {
+      totalVotos > 0 && setCondicionVotosTotales(true)
+    },[totalVotos])
+
 
     const recibirId = (id) => {
         setID(id);
         id === 'javier milei' ?
-        setVotosMilei(votosMilei + (Math.ceil(Math.random()*10))): 
+        setVotosMilei(votosMilei + 1): 
         setVotosMassa(votosMassa + 1);
       }
+
+      useEffect(() => {
+        totalVotos > 0 &&
+        setCondicionVotosTotales(true);
+        setTimeout(() => {
+          setCondicionVotosTotales(false);
+        }, 750);
+      },[totalVotos])
+
 
     return (
     <div className='bloque-resultados'>
@@ -38,14 +54,15 @@ const Pantallavotacion = () => {
           votosMassa = {votosMassa}/>
         ))}
       </div>
-       
+      <p className={condicionVotosTotales ? 'votostotales zoomearvotostotales' : 'votostotales'}>{totalVotos}</p>
         <>
           <div className='boletas-container'>
               {candidatos.map (candidato => (
                 <Votos 
                 boleta = {candidato.boleta}
                 nombreCandidato = {candidato.nombre}
-                enviarId = {recibirId}/>
+                enviarId = {recibirId}
+                enviarTotalVotos = {totalVotos}/>
               ))}
           </div>
         </>
